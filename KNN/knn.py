36d6b3d6c0c5a6ace6a21f6094620092f1245512
Bar <bar.cohen@siemens.com>
2020-10-06 10:03:15 +0300

    KNN - small vec upgrade

--- KNN/knn.py
+++ KNN/knn.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-from MLFun.DataSets.diabetes_preprocess import pre_process
 
 
 class KNNClassifer() :
@@ -18,13 +17,16 @@ class KNNClassifer() :
         self.X_train = X_train
         self.y_train = y_train
 
+    def euclidean_distance(self , sample1 , sample2 ):
+        return np.sqrt(np.sum(np.power(sample1 - sample2, 2)))
+
+
     def predict(self, X_test, k):
         rets = []
-        for sample in X_test:
-            args = np.argsort([abs(t-sample) for t in self.X_train])
+        for sample in X_test.values:
+            args = np.argsort([self.euclidean_distance(sample ,t ) for t in self.X_train.values])
             y_sorted = self.y_train[args][0:k]
             choice = 0 if np.sum(y_sorted) / len(y_sorted) < 0.5 else 1
-            print(choice)
             rets.append(choice)
         return rets
 
@@ -33,13 +35,21 @@ class KNNClassifer() :
         print(np.sum([1 for p,y in zip(pred, y_test) if p==y]) / len(pred))
         
 
+def pre_process(data_path):
+    df = pd.read_csv(data_path)
+    scaler = StandardScaler()
+    X = df.drop(columns='diabetes')
+    X = pd.DataFrame(scaler.fit_transform(X), index=X.index, columns=X.columns)
+    y = df['diabetes'].values
+    X_train, X_test ,y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.2, stratify=y)
+    return X_train, X_test ,y_train, y_test
 
 if __name__ == '__main__':
     X_train, X_test, y_train, y_test = pre_process('../DataSets/diabetes_data.csv')
     knn = KNNClassifer()
     knn.fit(X_train, y_train)
-    preds = knn.predict(X_train, k=1)
-    knn.getAccuracy(preds, y_train)
+    preds = knn.predict(X_test, k=5)
+    knn.getAccuracy(preds, y_test)
 
 
 
